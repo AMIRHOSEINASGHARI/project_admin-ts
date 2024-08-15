c2d7c9afa5147ce8478ba64d2ea32b1a216de621
amirhosein asghari <95590395+AMIRHOSEINASGHARI@users.noreply.github.com>
2024-08-15 14:32:26 +0330

    adding enum

---
+++ enums/index.ts
@@ -0,0 +1,11 @@
+export enum ResponseMessages {
+  USER_NOT_FOUND = "User Not Found!",
+  SERVER_ERROR = "Server Error!",
+  UN_AUTHORIZED = "Un Authorized!",
+  ACCESS_DENIED = "Access Denied!",
+  SUCCESSFULLY_FETCHED = "Successfully Fetched!",
+  SUCCESSFULLY_CREATED = "Successfully Created!",
+  SUCCESSFULLY_UPDATED = "Successfully Updated!",
+  SUCCESSFULLY_DELETED = "Successfully Deleted!",
+  SOMTHING_WENT_WRONG = "Somthing Went Wrong!",
+}
