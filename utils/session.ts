196a2b96eb963f2be1cc983473ce5b686d9baa4d
amirhosein asghari <95590395+AMIRHOSEINASGHARI@users.noreply.github.com>
2024-08-15 13:03:14 +0330

    adding session / connectDB / vars files

---
+++ utils/session.ts
@@ -0,0 +1,20 @@
+// next
+import { cookies } from "next/headers";
+// jwt
+import { verify } from "jsonwebtoken";
+
+const SECRET_KEY = process.env.SESSION_SECRET_KEY;
+
+export const getServerSession = (): any => {
+  try {
+    const cookieStore = cookies();
+    const accessToken = cookieStore?.get("accessToken")?.value;
+
+    const session = verify(accessToken!, SECRET_KEY!);
+    //! SECRET_KEY!
+
+    return session;
+  } catch (error) {
+    return null;
+  }
+};
