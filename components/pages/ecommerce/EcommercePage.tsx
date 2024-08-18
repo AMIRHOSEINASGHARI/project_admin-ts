8845b1e96405b720eb74f537655fee4378748251
amirhosein asghari <95590395+AMIRHOSEINASGHARI@users.noreply.github.com>
2024-08-18 21:18:54 +0330

    SaleByGender cmp

--- components/pages/ecommerce/EcommercePage.tsx
+++ components/pages/ecommerce/EcommercePage.tsx
@@ -1,10 +1,14 @@
 import View from "@/components/shared/layout/View";
 import EcommercePageCards from "./ui/EcommercePageCards";
+import SaleByGender from "./ui/SaleByGender";
 
 const EcommercePage = () => {
   return (
     <View orientation="vertical">
       <EcommercePageCards />
+      <View variant="flex-gap">
+        <SaleByGender />
+      </View>
     </View>
   );
 };
---
+++ components/pages/ecommerce/ui/SaleByGender.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+// cmp
+import { LabelList, RadialBar, RadialBarChart } from "recharts";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+
+const chartData = [
+  { gender: "kids", sales: 90, fill: "var(--color-kids)" },
+  { gender: "mens", sales: 180, fill: "var(--color-mens)" },
+  { gender: "womens", sales: 270, fill: "var(--color-womens)" },
+];
+
+const chartConfig = {
+  sales: {
+    label: "Sales",
+  },
+  kids: {
+    label: "Kids",
+    color: "#f87171",
+  },
+  mens: {
+    label: "Mens",
+    color: "#facc15",
+  },
+  womens: {
+    label: "Womens",
+    color: "#4ade80",
+  },
+} satisfies ChartConfig;
+
+const SaleByGender = () => {
+  return (
+    <Card className="flex flex-col w-full">
+      <CardHeader className="pb-0">
+        <CardTitle>Radial Chart</CardTitle>
+      </CardHeader>
+      <CardContent className="flex-1 pb-0">
+        <ChartContainer
+          config={chartConfig}
+          className="mx-auto aspect-square min-w-[200px] max-h-[250px]"
+        >
+          <RadialBarChart
+            data={chartData}
+            innerRadius={60}
+            outerRadius={120}
+            startAngle={-90}
+            endAngle={180}
+          >
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel nameKey="gender" />}
+            />
+            <RadialBar dataKey="sales" background cornerRadius={10}>
+              <LabelList
+                position="insideStart"
+                dataKey="gender"
+                className="fill-white capitalize mix-blend-luminosity"
+                fontSize={11}
+              />
+            </RadialBar>
+          </RadialBarChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default SaleByGender;
