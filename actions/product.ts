57767258e3d23d5cb56488901580b2231a5aa7ad
amirhosein asghari <95590395+AMIRHOSEINASGHARI@users.noreply.github.com>
2024-08-21 03:07:41 +0330

    getLatestProducts server action

---
+++ actions/product.ts
@@ -0,0 +1,36 @@
+"use server";
+
+// enums
+import { ResponseCodes, ResponseMessages } from "@/enums";
+// models
+import ProductModel from "@/models/product";
+// types
+import { ProductType } from "@/types/product";
+// utils
+import connectDB from "@/utils/connectDB";
+
+export const getLatestProducts = async () => {
+  try {
+    await connectDB();
+    const products = await ProductModel.find({
+      stock: { $gt: 0 },
+      published: true,
+    })
+      .limit(5)
+      .sort({ createdAt: -1 })
+      .lean<ProductType[]>();
+
+    return {
+      products,
+      message: ResponseMessages.SUCCESSFULLY_FETCHED,
+      code: ResponseCodes.SUCCESSFULLY_FETCHED,
+    };
+  } catch (error: any) {
+    console.log(error);
+    return {
+      tasks: null,
+      message: error.message,
+      code: ResponseCodes.SERVER_ERROR,
+    };
+  }
+};
