450b300aa5300cd73c6aedd4c29e0774177ef00b
amirhosein asghari <95590395+AMIRHOSEINASGHARI@users.noreply.github.com>
2024-08-26 20:09:40 +0330

    CustomBreadcrumb cmp

---
+++ components/shared/CustomBreadcrumb.tsx
@@ -0,0 +1,46 @@
+// react
+import { Fragment } from "react";
+// cmp
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+type CustomBreadcrumbProps = {
+  data: {
+    title: string;
+    href: string;
+  }[];
+  breadcrumbPage?: string;
+};
+
+const CustomBreadcrumb = ({ data, breadcrumbPage }: CustomBreadcrumbProps) => {
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        {data.map((item, index) => (
+          <Fragment key={item.title}>
+            <BreadcrumbItem>
+              <BreadcrumbLink href={item.href}>{item.title}</BreadcrumbLink>
+            </BreadcrumbItem>
+            {data.length <= index - 1 && <BreadcrumbSeparator />}
+          </Fragment>
+        ))}
+        {breadcrumbPage && (
+          <>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage>{breadcrumbPage}</BreadcrumbPage>
+            </BreadcrumbItem>
+          </>
+        )}
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+};
+
+export default CustomBreadcrumb;
