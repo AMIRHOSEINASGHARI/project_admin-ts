5372e14fe564fb104a4984599e21997431b8bffe
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2024-09-11 16:25:18 +0330

    SortBlogs cmp

--- components/pages/blogs/ui/SortBlogs.tsx
+++ components/pages/blogs/ui/SortBlogs.tsx
@@ -1,7 +1,37 @@
 "use client";
 
+// react
+import { useState } from "react";
+// cmp
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 const SortBlogs = () => {
-  return <div className="flex max-lg:justify-end">SortBlogs</div>;
+  const [open, setOpen] = useState<boolean>(false);
+
+  const onOpenChange = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <div className="flex max-lg:justify-end">
+      <Select defaultValue="Latest" open={open} onOpenChange={onOpenChange}>
+        <SelectTrigger className="h-fit min-w-[150px] flex flex-1 rounded-md border-none bg-white dark:bg-transparent dark:text-light3 text-sm">
+          <SelectValue placeholder="Sort by: Latest" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="Latest">{!open && "Sort by "}Latest</SelectItem>
+          <SelectItem value="Popular">{!open && "Sort by "}Popular</SelectItem>
+          <SelectItem value="Oldest">{!open && "Sort by "}Oldest</SelectItem>
+        </SelectContent>
+      </Select>
+    </div>
+  );
 };
 
 export default SortBlogs;
