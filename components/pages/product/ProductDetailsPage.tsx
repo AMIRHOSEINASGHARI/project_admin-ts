9ab44a86d7d467ec8b4824a73d45df2d4144e6e5
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2024-09-25 04:38:15 +0330

    HeadingActions cmp in ProductDetailsPage

--- components/pages/product/ProductDetailsPage.tsx
+++ components/pages/product/ProductDetailsPage.tsx
@@ -1,5 +1,29 @@
+// next
+import Link from "next/link";
+import { notFound } from "next/navigation";
+// actions
+import { getProduct } from "@/actions/product";
+// icons
+import { SolarAltArrowLeftLineDuotone } from "@/components/svg";
+// cmp
+import HeadingActions from "./ui/HeadingActions";
+
 const ProductDetailsPage = async ({ id }: { id: string }) => {
-  return <div>ProductDetailsPage {id}</div>;
+  const data = await getProduct(id);
+
+  if (!data?.product) notFound();
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Link href="/products" className="back-link">
+          <SolarAltArrowLeftLineDuotone />
+          <span>Back</span>
+        </Link>
+        <HeadingActions id={id} published={data?.product?.published} />
+      </div>
+    </>
+  );
 };
 
 export default ProductDetailsPage;
---
+++ components/pages/product/ui/HeadingActions.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+// react
+import { useState } from "react";
+// next
+import Link from "next/link";
+// icons
+import {
+  SolarCloudUploadBoldDuotone,
+  SolarDocumentsBoldDuotone,
+  SolarPenBoldDuotone,
+} from "@/components/svg";
+// cmp
+import CustomTooltip from "@/components/shared/CustomTooltip";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+} from "@/components/ui/select";
+import { Button } from "@/components/ui/button";
+
+type ProductStatus = "Published" | "Draft";
+type HeadingActionsProps = {
+  id: string;
+  published: boolean;
+};
+
+const HeadingActions = ({ id, published }: HeadingActionsProps) => {
+  const [open, setOpen] = useState(false);
+  const [status, setStatus] = useState<"Published" | "Draft">(
+    published ? "Published" : "Draft"
+  );
+
+  const onOpenChange = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <div className="flex items-center gap-3">
+      <CustomTooltip
+        trigger={
+          <Button asChild variant="icon">
+            <Link href={`/products/${id}/edit`}>
+              <SolarPenBoldDuotone />
+            </Link>
+          </Button>
+        }
+        content="Edit"
+      />
+      <Select
+        open={open}
+        onOpenChange={onOpenChange}
+        value={status}
+        onValueChange={(value: ProductStatus) => setStatus(value)}
+      >
+        <SelectTrigger className="gap-2 bg-dark1 text-white hover:bg-dark3 hover:text-white dark:bg-white dark:text-black dark:hover:bg-light3 dark:hover:text-black Transition">
+          {status}
+        </SelectTrigger>
+        <SelectContent className="min-w-[170px]">
+          <SelectItem value="Published">
+            <div className="flex items-center gap-2">
+              <SolarCloudUploadBoldDuotone className="text-icon-size" />{" "}
+              Published
+            </div>
+          </SelectItem>
+          <SelectItem value="Draft">
+            <div className="flex items-center gap-2">
+              <SolarDocumentsBoldDuotone className="text-icon-size" /> Draft
+            </div>
+          </SelectItem>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
+
+export default HeadingActions;
