17250356ac3ce82bb7d8979ce6ecb3ea754bdc04
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2024-09-27 10:04:34 +0330

    changing blog type and model

--- models/blog.ts
+++ models/blog.ts
@@ -4,16 +4,21 @@ import { Schema, model, models } from "mongoose";
 const blogSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  image: { type: String, required: true },
-  keywords: { type: [String], default: [] },
+  content: { type: String, required: true },
+  cover: { type: String, required: true },
+  tags: { type: [String], default: [], required: true },
   likes: [{ type: Schema.Types.ObjectId, ref: "Like", default: [] }],
   published: { type: Boolean, default: false },
+  enableComments: { type: Boolean, default: false },
   createdBy: { type: Schema.Types.ObjectId, ref: "Admin" },
   createdAt: {
     type: Date,
     default: () => Date.now(),
     immutabale: true,
   },
+  metaTitle: { type: String, required: true },
+  metaDescription: { type: String, required: true },
+  metaKeywords: { type: [String], default: [], required: true },
 });
 
 const BlogModel = models?.Blog || model("Blog", blogSchema);
