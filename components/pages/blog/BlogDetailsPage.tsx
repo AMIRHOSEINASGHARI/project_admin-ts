4964293b642cd19c3856f30010811649694df2fd
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2024-09-28 07:00:37 +0330

    using getBlog server action in BlogDetailsPage

--- components/pages/blog/BlogDetailsPage.tsx
+++ components/pages/blog/BlogDetailsPage.tsx
@@ -1,12 +1,19 @@
 // next
 import Link from "next/link";
+import { notFound } from "next/navigation";
+// actions
+import { getBlog } from "@/actions/blogs";
 // icons
 import { SolarAltArrowLeftLineDuotone } from "@/components/svg";
 // cmp
 import View from "@/components/shared/layout/View";
 import BlogHeadingActions from "./ui/BlogHeadingActions";
 
-const BlogDetailsPage = ({ id }: { id: string }) => {
+const BlogDetailsPage = async ({ id }: { id: string }) => {
+  const data = await getBlog(id);
+
+  if (!data?.blog) notFound();
+
   return (
     <View className="space-y-10">
       <div className="flex items-center justify-between">
