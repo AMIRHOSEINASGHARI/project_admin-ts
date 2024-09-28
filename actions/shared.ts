62d3b2cac6caf9984151d7d4496d5bf47480ce7c
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2024-09-28 06:43:43 +0330

    checkSession server action

---
+++ actions/shared.ts
@@ -0,0 +1,30 @@
+"use server";
+
+// enums
+import { ResponseMessages } from "@/enums";
+// models
+import AdminModel from "@/models/admin";
+// utils
+import connectDB from "@/utils/connectDB";
+import { getServerSession } from "@/utils/session";
+
+export const checkSession = async () => {
+  try {
+    await connectDB();
+
+    const session = getServerSession();
+
+    if (!session) {
+      throw new Error(ResponseMessages.UN_AUTHORIZED);
+    }
+
+    const currentUser = await AdminModel.findById(session?.userId);
+
+    if (currentUser?.roll === "USER") {
+      throw new Error(ResponseMessages.ACCESS_DENIED);
+    }
+  } catch (error: any) {
+    console.log(error);
+    throw new Error(`Cannot check user at this time:`, error);
+  }
+};
