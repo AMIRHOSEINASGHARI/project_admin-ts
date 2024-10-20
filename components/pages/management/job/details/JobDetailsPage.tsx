b7474652b237451bc96181860570b793d144d444
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2024-10-20 15:10:21 +0330

    JobHeadingActions cmp in job details page

--- components/pages/management/job/details/JobDetailsPage.tsx
+++ components/pages/management/job/details/JobDetailsPage.tsx
@@ -1,5 +1,35 @@
-const JobDetailsPage = ({ id }: { id: string }) => {
-  return <div>{id}</div>;
+// next
+import Link from "next/link";
+import { notFound } from "next/navigation";
+// actions
+import { getJob } from "@/actions/job";
+// icons
+import { SolarAltArrowLeftLineDuotone } from "@/components/svg";
+// cmp
+import JobHeadingActions from "./ui/JobHeadingActions";
+import { Button } from "@/components/ui/button";
+
+const JobDetailsPage = async ({ id }: { id: string }) => {
+  const data = await getJob(id);
+
+  if (!data?.job) notFound();
+
+  return (
+    <div>
+      <div className="flex items-center justify-between">
+        <Button asChild variant="action">
+          <Link href="/product/list">
+            <SolarAltArrowLeftLineDuotone />
+            Back
+          </Link>
+        </Button>
+        <JobHeadingActions
+          id={id}
+          published={data?.job?.properties?.published}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default JobDetailsPage;
---
+++ components/pages/management/job/details/ui/JobHeadingActions.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+// react
+import { useState } from "react";
+// next
+import Link from "next/link";
+// icons
+import {
+  SolarCloudUploadBoldDuotone,
+  SolarDocumentsBoldDuotone,
+  SolarPenBoldDuotone,
+} from "@/components/svg";
+// cmp
+import CustomTooltip from "@/components/shared/CustomTooltip";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+} from "@/components/ui/select";
+import { Button } from "@/components/ui/button";
+
+type JobStatus = "Published" | "Draft";
+type JobHeadingActionsProps = {
+  id: string;
+  published: boolean;
+};
+
+const JobHeadingActions = ({ id, published }: JobHeadingActionsProps) => {
+  const [open, setOpen] = useState(false);
+  const [status, setStatus] = useState<"Published" | "Draft">(
+    published ? "Published" : "Draft"
+  );
+
+  const onOpenChange = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <div className="flex items-center gap-3">
+      <CustomTooltip
+        trigger={
+          <Button asChild variant="icon">
+            <Link href={`/job/${id}/edit`}>
+              <SolarPenBoldDuotone />
+            </Link>
+          </Button>
+        }
+        content="Edit"
+      />
+      <Select
+        open={open}
+        onOpenChange={onOpenChange}
+        value={status}
+        onValueChange={(value: JobStatus) => setStatus(value)}
+      >
+        <SelectTrigger className="gap-2 bg-dark1 text-white hover:bg-dark3 hover:text-white dark:bg-white dark:text-black dark:hover:bg-light3 dark:hover:text-black Transition">
+          {status}
+        </SelectTrigger>
+        <SelectContent className="min-w-[170px]">
+          <SelectItem value="Published">
+            <div className="flex items-center gap-2">
+              <SolarCloudUploadBoldDuotone className="text-icon-size" />{" "}
+              Published
+            </div>
+          </SelectItem>
+          <SelectItem value="Draft">
+            <div className="flex items-center gap-2">
+              <SolarDocumentsBoldDuotone className="text-icon-size" /> Draft
+            </div>
+          </SelectItem>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
+
+export default JobHeadingActions;
