e4f23c66d340a45ca28ac3d6e2cc49e3fa0081ab
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2024-10-21 10:25:39 +0330

    SearchTours cmp / FilterTours cmp

--- components/pages/management/tour/list/ui/FilterTours.tsx
+++ components/pages/management/tour/list/ui/FilterTours.tsx
@@ -1,7 +1,76 @@
-import React from "react";
+"use client";
+
+// react
+import { useState } from "react";
+// cmp
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import * as VisuallyHidden from "@radix-ui/react-visually-hidden";
+// icons
+import { Separator } from "@/components/ui/separator";
+// icons
+import { CrossRegular } from "@/components/svg";
+
+// icons
+import { SolarFilterBoldDuotone } from "@/components/svg";
+// cmp
+import { Button } from "@/components/ui/button";
+
+const HiddenTags = () => (
+  <VisuallyHidden.Root>
+    <SheetHeader>
+      <SheetTitle></SheetTitle>
+      <SheetDescription></SheetDescription>
+    </SheetHeader>
+  </VisuallyHidden.Root>
+);
 
 const FilterTours = () => {
-  return <div>FilterTours</div>;
+  const [open, setOpen] = useState(false);
+
+  const onOpenChange = () => {
+    setOpen(!open);
+  };
+  const onClose = () => {
+    setOpen(false);
+  };
+  const onOpen = () => {
+    setOpen(true);
+  };
+
+  const sheetContent = (
+    <div>
+      <div className="flex items-center justify-between p-4">
+        <span className="bold-value-2 dark:text-light1">Settings</span>
+        <Button variant="icon" onClick={onClose} className="text-small">
+          <CrossRegular />
+        </Button>
+      </div>
+      <Separator />
+      {/* // TODO: Filter tours */}
+      <div className="p-4"></div>
+    </div>
+  );
+
+  return (
+    <Sheet open={open} onOpenChange={onOpenChange}>
+      <div className="flex max-xl:justify-end max-xl:w-full">
+        <Button type="button" variant="action" onClick={onOpen}>
+          <span>Filters</span>
+          <SolarFilterBoldDuotone className="text-icon-light dark:text-icon-dark text-lg" />
+        </Button>
+      </div>
+      <SheetContent className="p-0">
+        <HiddenTags />
+        {sheetContent}
+      </SheetContent>
+    </Sheet>
+  );
 };
 
 export default FilterTours;
--- components/pages/management/tour/list/ui/SearchTours.tsx
+++ components/pages/management/tour/list/ui/SearchTours.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+// icons
+import { SolarMinimalisticMagniferBoldDuotone } from "@/components/svg";
+// cmp
+import { Input } from "@/components/ui/input";
 
 const SearchTours = () => {
-  return <div>SearchTours</div>;
+  return (
+    <div className="relative w-full xl:w-[300px]">
+      <Input placeholder="Search..." className="w-full pl-[40px]" />
+      <SolarMinimalisticMagniferBoldDuotone className="icon absolute top-[14px] left-3" />
+    </div>
+  );
 };
 
 export default SearchTours;
