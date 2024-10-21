c0449c9ea4e13b9c737c6d8bb90c8c92e5f9f8d6
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2024-10-21 10:05:06 +0330

    createTour server action

---
+++ actions/tour.ts
@@ -0,0 +1,48 @@
+"use server";
+
+// next
+import { revalidatePath } from "next/cache";
+// enums
+import { ResponseCodes, ResponseMessages } from "@/enums";
+// models
+import TourModel from "@/models/tour";
+// types
+import { CreateTour, TourType } from "@/types/tour";
+// actions
+import { checkSession } from "./shared";
+// utils
+import connectDB from "@/utils/connectDB";
+
+export const createTour = async (data: CreateTour) => {
+  try {
+    await checkSession();
+
+    const {
+      name,
+      content,
+      images,
+      tourGuide,
+      startDate,
+      endDate,
+      duration,
+      destination,
+      services,
+      tags,
+      published,
+      price,
+      discount,
+    } = data;
+
+    await TourModel.create(data);
+
+    revalidatePath("/tour");
+
+    return {
+      message: ResponseMessages.SUCCESSFULLY_CREATED,
+      code: ResponseCodes.SUCCESSFULLY_CREATED,
+    };
+  } catch (error) {
+    console.log(error);
+    throw new Error(ResponseMessages.SERVER_ERROR);
+  }
+};
