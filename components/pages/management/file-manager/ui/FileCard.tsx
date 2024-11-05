9f9a431ae6a14c3bd4f3f7605e12ebfb4ce99941
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2024-11-05 16:17:07 +0330

    FileCard cmp

--- components/pages/management/file-manager/ui/FileCard.tsx
+++ components/pages/management/file-manager/ui/FileCard.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+// types
+import { File } from "@/types/shared";
+// icons
+import {
+  SolarOverflowMenuVertical,
+  StarFill,
+  StarRegular,
+} from "@/components/svg";
+// cmp
+import { Button } from "@/components/ui/button";
+import moment from "moment";
 
-const FileCard = () => {
-  return <div>FileCard</div>;
+const FileCard = ({
+  file: { icon, important, size, name, modified },
+}: {
+  file: File;
+}) => {
+  return (
+    <div className="flex flex-col gap-2 min-w-[250px] border border-border-light dark:border-border-dark p-[20px] rounded-card hover:shadow-2xl Transition hover:shadow-gray-200 dark:hover:bg-dark3 dark:hover:shadow-black/30 cursor-pointer">
+      <div className="flex items-start justify-between">
+        <div className="text-[36px]">{icon}</div>
+        <div className="flex items-center gap-1">
+          <Button variant="icon" className="text-xl">
+            {important ? (
+              <StarFill className="text-yellow-500" />
+            ) : (
+              <StarRegular />
+            )}
+          </Button>
+          <Button variant="icon" className="text-xl">
+            <SolarOverflowMenuVertical />
+          </Button>
+        </div>
+      </div>
+      <span className="bold-value-3">{name}</span>
+      <span className="text_disabled">
+        {size} / {moment(modified).format("lll")}
+      </span>
+    </div>
+  );
 };
 
 export default FileCard;
--- components/pages/management/file-manager/ui/FilesList.tsx
+++ components/pages/management/file-manager/ui/FilesList.tsx
@@ -20,14 +20,14 @@ const Folders = () => {
   const folders = files.filter((file) => file.type === "folder");
 
   return (
-    <div className="space-y-4">
+    <div className="space-y-6">
       <div className="flex flex-col gap-1">
         <h5 className="h5">Folders</h5>
         <span className="text_disabled">{folders.length} folders</span>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-5">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-8">
         {folders.map((item) => (
-          <FileCard key={item.id} />
+          <FileCard key={item.id} file={item} />
         ))}
       </div>
     </div>
@@ -38,14 +38,14 @@ const Files = () => {
   const otherFiles = files.filter((file) => file.type !== "folder");
 
   return (
-    <div className="space-y-4">
+    <div className="space-y-6">
       <div className="flex flex-col gap-1">
         <h5 className="h5">Files</h5>
         <span className="text_disabled">{otherFiles.length} files</span>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-5">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-8">
         {otherFiles.map((item) => (
-          <FileCard key={item.id} />
+          <FileCard key={item.id} file={item} />
         ))}
       </div>
     </div>
