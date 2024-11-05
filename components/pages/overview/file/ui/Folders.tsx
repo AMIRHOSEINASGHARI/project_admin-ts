02e15efd536823dd561b5a087cd0200e33b8894e
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2024-11-05 16:19:07 +0330

    ...

--- components/pages/overview/file/ui/Folders.tsx
+++ components/pages/overview/file/ui/Folders.tsx
@@ -2,14 +2,8 @@
 import { files } from "@/constants";
 // cmp
 import CardAttatchmentButton from "@/components/shared/CardAttatchmentButton";
-import {
-  SolarOverflowMenuVertical,
-  StarFill,
-  StarRegular,
-} from "@/components/svg";
-import { Button } from "@/components/ui/button";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
-import moment from "moment";
+import FileCard from "@/components/pages/management/file-manager/ui/FileCard";
 
 const Folders = () => {
   return (
@@ -22,31 +16,8 @@ const Folders = () => {
         <div className="flex w-max space-x-4 mb-5">
           {files
             .filter((item) => item.type === "folder")
-            .map(({ id, icon, important, modified, name, size }) => (
-              <div
-                key={id}
-                className="flex flex-col gap-2 min-w-[250px] border border-border-light dark:border-border-dark p-[20px] rounded-card"
-              >
-                <div className="flex items-start justify-between">
-                  <div className="text-[36px]">{icon}</div>
-                  <div className="flex items-center gap-1">
-                    <Button variant="icon" className="text-xl">
-                      {important ? (
-                        <StarFill className="text-yellow-500" />
-                      ) : (
-                        <StarRegular />
-                      )}
-                    </Button>
-                    <Button variant="icon" className="text-xl">
-                      <SolarOverflowMenuVertical />
-                    </Button>
-                  </div>
-                </div>
-                <span className="bold-value-3">{name}</span>
-                <span className="text_disabled">
-                  {size} / {moment(modified).format("lll")}
-                </span>
-              </div>
+            .map((file) => (
+              <FileCard key={file.id} file={file} />
             ))}
         </div>
         <ScrollBar orientation="horizontal" />
