803b8c0995aa0425634d7913226a44d6fc1468ad
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2024-11-09 18:34:43 +0330

    ...

--- app/(pages)/(management)/user/cards/page.tsx
+++ app/(pages)/(management)/user/cards/page.tsx
@@ -3,6 +3,8 @@ import { UsersListParams } from "@/types/admin";
 // cmp
 import UserCardsPage from "@/components/pages/management/user/cards/UserCardsPage";
 
+export const dynamic = "force-dynamic";
+
 const UserCards = ({ searchParams }: { searchParams: UsersListParams }) => {
   return <UserCardsPage searchParams={searchParams} />;
 };
