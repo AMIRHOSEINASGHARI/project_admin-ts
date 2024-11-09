dd12f76a199bf2feae27e340a24dce68dcf38009
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2024-11-09 17:16:52 +0330

    handling search param in SearchUser cmp in user list page

--- components/pages/management/user/list/ui/SearchUser.tsx
+++ components/pages/management/user/list/ui/SearchUser.tsx
@@ -1,5 +1,11 @@
 "use client";
 
+// react
+import { useEffect, useState } from "react";
+// hooks
+import { useHandleSearchParams } from "@/hooks";
+// debounce
+import { useDebouncedCallback } from "use-debounce";
 // icons
 import {
   SolareExportBoldDuotone,
@@ -18,10 +24,37 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 const SearchUser = () => {
+  const { handleSetQuery, searchParams } = useHandleSearchParams("search");
+  const [query, setQuery] = useState(
+    searchParams.get("search")?.toString() || ""
+  );
+
+  const handleSearch = useDebouncedCallback((term: string) => {
+    handleSetQuery(term);
+  }, 300);
+
+  useEffect(() => {
+    const params = new URLSearchParams(searchParams);
+
+    if (!params.has("search")) {
+      setQuery("");
+    } else {
+      setQuery(searchParams.get("search")?.toString()!);
+    }
+  }, [searchParams]);
+
   return (
     <div className="w-full xl:w-[70%] flex items-center justify-between gap-5">
       <div className="relative w-full">
-        <Input placeholder="Search..." className="w-full pl-[40px]" />
+        <Input
+          placeholder="Search..."
+          className="w-full pl-[40px]"
+          value={query}
+          onChange={(e) => {
+            setQuery(() => e.target.value);
+            handleSearch(e.target.value);
+          }}
+        />
         <SolarMinimalisticMagniferBoldDuotone className="icon absolute top-[14px] left-3" />
       </div>
       <DropdownMenu>
