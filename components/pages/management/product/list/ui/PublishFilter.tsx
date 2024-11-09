fc4aecf8efa1c30b8fcb9ce77339602c77237195
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2024-11-09 11:42:51 +0330

    handing publish filter in PublishFilter cmp

--- components/pages/management/product/list/ui/PublishFilter.tsx
+++ components/pages/management/product/list/ui/PublishFilter.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+// react
+import { useEffect, useState } from "react";
+// next
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
 // cmp
 import {
   Select,
@@ -15,13 +19,41 @@ import {
 } from "@/components/svg";
 
 const PublishFilter = () => {
+  const [publish, setPublish] = useState("");
+
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { push } = useRouter();
+
+  const handlePublish = (publish: string) => {
+    const params = new URLSearchParams(searchParams);
+
+    if (publish) {
+      params.set("published", publish);
+    } else {
+      params.delete("published");
+    }
+
+    push(`${pathname}?${params.toString()}`);
+  };
+
+  useEffect(() => {
+    const params = new URLSearchParams(searchParams);
+
+    if (!params.has("published")) {
+      setPublish("");
+    } else {
+      setPublish(searchParams.get("published")?.toString()!);
+    }
+  }, [searchParams]);
+
   return (
-    <Select>
+    <Select onValueChange={(e) => handlePublish(e)} value={publish}>
       <SelectTrigger className="py-[15px] px-[14px] flex flex-1 rounded-md border border-slate-200 bg-white dark:bg-transparent dark:text-light3 text-sm">
         <SelectValue placeholder="Publish" />
       </SelectTrigger>
       <SelectContent>
-        <SelectItem value="published">
+        <SelectItem value="publish">
           <div className="flex items-center gap-2">
             <div className="text-xl text-icon-light dark:text-icon-dark">
               <SolarCloudUploadBoldDuotone />
