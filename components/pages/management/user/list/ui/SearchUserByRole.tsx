4645795664ffb9ea03cca69c68475fdadd3f374a
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2024-11-09 17:15:06 +0330

    SearchUserByRole cmp in user list page

--- components/pages/management/user/list/ui/SearchUserByRole.tsx
+++ components/pages/management/user/list/ui/SearchUserByRole.tsx
@@ -1,9 +1,43 @@
 "use client";
 
+// react
+import { useEffect, useState } from "react";
+// hooks
+import { useHandleSearchParams } from "@/hooks";
+// debounce
+import { useDebouncedCallback } from "use-debounce";
+// cmp
 import { Input } from "@/components/ui/input";
 
 const SearchUserByRole = () => {
-  return <Input placeholder="Role" className="w-full xl:w-[30%]" />;
+  const { handleSetQuery, searchParams } = useHandleSearchParams("role");
+  const [role, setRole] = useState(searchParams.get("role")?.toString() || "");
+
+  const handleRole = useDebouncedCallback((term: string) => {
+    handleSetQuery(term);
+  }, 300);
+
+  useEffect(() => {
+    const params = new URLSearchParams(searchParams);
+
+    if (!params.has("role")) {
+      setRole("");
+    } else {
+      setRole(searchParams.get("role")?.toString()!);
+    }
+  }, [searchParams]);
+
+  return (
+    <Input
+      placeholder="Role"
+      value={role}
+      onChange={(e) => {
+        setRole(() => e.target.value);
+        handleRole(e.target.value);
+      }}
+      className="w-full xl:w-[30%]"
+    />
+  );
 };
 
 export default SearchUserByRole;
