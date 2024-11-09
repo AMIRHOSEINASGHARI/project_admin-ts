d0e0967a8de06d4ee71dd2f8f00717bccb0f9960
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2024-11-09 18:32:01 +0330

    ...

--- app/(pages)/(management)/user/cards/page.tsx
+++ app/(pages)/(management)/user/cards/page.tsx
@@ -3,11 +3,7 @@ import { UsersListParams } from "@/types/admin";
 // cmp
 import UserCardsPage from "@/components/pages/management/user/cards/UserCardsPage";
 
-const UserCards = ({
-  searchParams,
-}: {
-  searchParams: Promise<UsersListParams>;
-}) => {
+const UserCards = ({ searchParams }: { searchParams: UsersListParams }) => {
   return <UserCardsPage searchParams={searchParams} />;
 };
 
