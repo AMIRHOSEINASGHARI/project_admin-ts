f3c4f77d1d3ae0baa9a642c0bee20b95eefde55f
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2024-11-11 14:57:53 +0330

    handling queries in  BlogsSearchTextFilter cmp

--- components/pages/management/blog/list/ui/BlogsSearchTextFilter.tsx
+++ components/pages/management/blog/list/ui/BlogsSearchTextFilter.tsx
@@ -1,14 +1,47 @@
 "use client";
 
+// react
+import { useEffect, useState } from "react";
+// hooks
+import { useHandleSearchParams } from "@/hooks";
+// debounce
+import { useDebouncedCallback } from "use-debounce";
 // cmp
 import { Input } from "@/components/ui/input";
 // icons
 import { SolarMinimalisticMagniferBoldDuotone } from "@/components/svg";
 
 const BlogsSearchTextFilter = () => {
+  const { handleSetQuery, searchParams } = useHandleSearchParams("search");
+  const [query, setQuery] = useState(
+    searchParams.get("search")?.toString() || ""
+  );
+
+  const handleSearch = useDebouncedCallback((term: string) => {
+    handleSetQuery(term);
+  }, 300);
+
+  useEffect(() => {
+    const params = new URLSearchParams(searchParams);
+
+    if (!params.has("search")) {
+      setQuery("");
+    } else {
+      setQuery(searchParams.get("search")?.toString()!);
+    }
+  }, [searchParams]);
+
   return (
     <div className="relative flex w-full lg:w-[300px]">
-      <Input placeholder="Search..." className="w-full h-full pl-[45px]" />
+      <Input
+        placeholder="Search..."
+        className="w-full h-full pl-[45px]"
+        value={query}
+        onChange={(e) => {
+          setQuery(() => e.target.value);
+          handleSearch(e.target.value);
+        }}
+      />
       <div className="absolute top-[13px] left-[15px] icon">
         <SolarMinimalisticMagniferBoldDuotone />
       </div>
